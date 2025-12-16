9c7f23b201bbada46e4ebf472f4a5646be43d966
dgerlo87 <116730222+dgerlo87@users.noreply.github.com>
2025-12-15 22:35:53 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,17 @@ export default function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       {/* Aquí ponemos el logo */}
-      <Image src="/IMG-20251215-WA0048.jpg alt="Mi logo" width={150} />
+      // Código que debes usar AHORA:
+<Image
+    src={"/IMG_20251215-WA0048.JPG"}
+    alt="Mi logo"
+    width={150}
+    height={150} // Los componentes Image de Next.js requieren 'height'
+/>
+
       
       {/* Texto de la página */}
       <h1>Bienvenido a mi página de fitness</h1>
       <p>¡Vamos a crear contenido increíble!</p>
     </div>
   );
-}

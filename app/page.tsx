0ed8c8ffa6748db2ac6da90bc5203b8f7069e3eb
dgerlo87 <116730222+dgerlo87@users.noreply.github.com>
2025-12-15 21:29:06 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
       {/* Aquí ponemos el logo */}
-      <img src="/logo.png" alt="Mi logo" width={150} />
+      <img src="/public/Screenshot_20251215-185613.png" alt="Mi logo" width={150} />
       
       {/* Texto de la página */}
       <h1>Bienvenido a mi página de fitness</h1>
